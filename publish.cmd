8dc06758dc006da6cbebfe94f3d2b4ea429a9df9
Yury Golub <yuragolub21@gmail.com>
2023-08-06 11:33:22 +0300

    Add publish script.

---
+++ publish.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+where /q dotnet
+if %ERRORLEVEL% NEQ 0 goto missingDotnet
+
+dotnet publish MidiApp --configuration Release --output publish -p:DebugType=None -p:DebugSymbols=false -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --no-self-contained --os win
+
+goto exit
+
+:missingDotnet
+echo dotnet was not found.
+echo You have to install .NET Runtime 7.0 and .NET Desktop Runtime 7.0 to run this application.
+echo You can download it here https://dotnet.microsoft.com/en-us/download/dotnet/7.0
+goto exit
+
+:exit
